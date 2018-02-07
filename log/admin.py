08a016eb0c964cb2812a282f3cfca76d496fc5ba
xiong <zxiong@fiberhome.com>
2018-02-07 21:26:13 +0800

    update model

--- log/admin.py
+++ log/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Log_type, Log
 
-# Register your models here.
+
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'person', 'start_time', 'region', 'detail')
+
+
+class Log_typeAdmin(admin.ModelAdmin):
+    list_display = ('type',)
+
+
+admin.site.register(Log, LogAdmin)
+admin.site.register(Log_type, Log_typeAdmin)
+
